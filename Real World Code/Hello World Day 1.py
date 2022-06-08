163ca655255a79af45db9fe3660146a4db681251
Alejandro Moreno <98673485+Amoreno817@users.noreply.github.com>
2022-06-07 21:10:24 -0400

    Add files via upload

---
+++ Real World Code/Hello World Day 1.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+print("Welcome to my awesome python project, get ready to be amazed!")
+#This is my first homework!
+def Age():
+    print("I am", 81792, "years old")
+def Name():
+    print("My name is Dr.Sir Master Sensei Alejandro PhD MD etc.")
+def CoolStatus():
+    print("Very Cool")
+    return True
+if "Cool" in "Very Cool!":
+    print ("You are the best!")
+else:
+    print("No,you are not there yet...")
+CoolStatus()
+Age()
+Name()
+Power_level = "Nine Thousand!"
+if "Nine Thousand" in Power_level:
+    print("Whoa, his power level is over",Power_level)
